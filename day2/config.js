bbf97361ce77e0792dc2d8483f58ce2be38f73f7
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-11-10 13:29:20 -0500

    Initial commit

---
+++ day2/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientId: '3a095ead-a361-4c33-b01b-2ad95225e9b1', // Make sure to set your environment up correctly when starting/building
+  scope: 'patient/Patient.read patient/Condition.read',
+  iss: 'https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca',
+  redirectUri: 'http://localhost:3000/',
+};
